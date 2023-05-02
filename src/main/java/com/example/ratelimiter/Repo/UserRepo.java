fdc75dd6f3027b5f1359857566066d39958fcb79
Niyonsengaeric <niyeric11@gmail.com>
2023-05-02 16:06:40 +0200

    ft: user login
    - add user login

--- src/main/java/com/example/ratelimiter/Repo/UserRepo.java
+++ src/main/java/com/example/ratelimiter/Repo/UserRepo.java
@@ -1,9 +1,10 @@
 package com.example.ratelimiter.Repo;
 
-
 import com.example.ratelimiter.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
     boolean existsByUserNameOrEmail(String userName, String email);
+
+    User findByUserNameOrEmail(String username, String username1);
 }
---
+++ src/main/java/com/example/ratelimiter/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,86 @@
+package com.example.ratelimiter.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.ratelimiter.Service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private final AuthenticationManager authenticationManager;
+
+	@Autowired
+	Environment env;
+
+	@Autowired
+	UserService userService;
+
+	public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		String username = request.getParameter("userName");
+		String password = request.getParameter("password");
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+				password);
+		return authenticationManager.authenticate(authenticationToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authentication) throws IOException, ServletException {
+		User user = (User) authentication.getPrincipal();
+		Algorithm algorithm = Algorithm.HMAC256("secretKey".getBytes());
+		String access_token = JWT.create().withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+				.withIssuer(request.getRequestURL().toString())
+				.withClaim("roles",
+						user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+				.sign(algorithm);
+
+		String refresh_token = JWT.create().withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() - 30 * 24 * 60 * 60 * 1000))
+				.withIssuer(request.getRequestURL().toString()).sign(algorithm);
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+		Map<String, String> data = new HashMap<>();
+		data.put("access_token", access_token);
+		request.setAttribute("userName", user.getUsername());
+		request.setAttribute("data", data);
+
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+		super.unsuccessfulAuthentication(request, response, failed);
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+				failed.getMessage());
+		new ObjectMapper().writeValue(response.getOutputStream(), failed);
+
+	}
+}
